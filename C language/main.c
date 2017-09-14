01eefd3ab1914918b2de3231ec5a2a47c69662c6
yatakan <takaya423@outlook.jp>
2017-09-14 20:50:14 +0900

    Initial Commit

---
+++ C language/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C language
+//
+//  Created by taka on 2017/09/14.
+//  Copyright © 2017年 takayan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
